4799247ec3ed8557b2517d2096cc974e5d69b778
Adgaur027 <121216575+AdityaGaur7@users.noreply.github.com>
2023-05-22 00:12:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
+
+/*let btn = document.getElementById("no");*/
+
 let cnt=0;
+
 let lft=0;
+
 let tp=0;
 
 
@@ -9,10 +14,12 @@ var f1 = ()=>{
     
     
 // mov=mov+5;
+    
 // cnt=cnt+5;
     
     
 cnt=cnt+10;
+    
 if(cnt<25){
     lft=lft+5;
 
@@ -26,19 +33,24 @@ if(cnt<25){
     document.getElementById("no").style.left=lft+'vw';
 
     // document.getElementById("no").style.right=rgt+'vw';
+    
     // document.getElementById("no").style.bottom=btm+'vh';
    
     
     
     
 }else if(cnt>=25 && cnt<=50){
+    
     tp=tp+5;
     
     
     document.getElementById("no").style.top=tp+'vh';
+    
 }
 else if(cnt>50 && cnt<75){
+    
     lft=lft-5;
+    
     document.getElementById("no").style.left=-lft+'vw';
 }
 else if(cnt>=75  && cnt<100){
@@ -47,6 +59,8 @@ else if(cnt>=75  && cnt<100){
 }
 else if(cnt>100){
     cnt=0;
+  
+    
 }
     
     
@@ -85,17 +99,27 @@ let f2=()=>{
     
 }
 // let t =0;
+
 // let f2=()=>{
+
 //     setInterval(() => {
+
 //         t = t+1;
 //     }, 1000);
+
+
 //     if(t>=5 && t<10){
+
 //         lisa1.src="img/crush1.gif";
+
 //     }else if(t>=10 && t<20)
 //     {
+
 //         lisa1.src="img/crush4.gif";
 //     }
+
 // }
+
 
 
 
@@ -105,14 +129,28 @@ let f2=()=>{
 
 // function f2() {
 
+
 //     let time = 0;
+
 //     setInterval(() => {
+
 //         time++;
+
 //     }, 1000);
+
+
 //     if (time > 3) {
+
 //         lisa1.src = "img/crush1.gif";
 //     }
 // }
+
+
+
+
+
+
+
 
 
 
